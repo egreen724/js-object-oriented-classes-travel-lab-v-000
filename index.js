3fbbb043b74d1ce7618d91029d51fbd59aad81ec
Erin Green <erin.y.green@gmail.com>
2019-05-15 17:34:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ class Driver {
     let end = new Date(endDate, 1, 1)
     let totalYears = Math.floor((end - this.startDate) / (365 * 24 * 60 * 60 * 1000));
     return totalYears
-
   }
 }
 
@@ -26,6 +25,10 @@ class Route {
   constructor(beginningLocation, endingLocation) {
     this.beginningLocation = beginningLocation
     this.endingLocation = endingLocation
+  }
+
+  avenueToInteger(avenue) {
+    return eastWest.indexOf(avenue)
   }
 
   blocksTravelled() {
