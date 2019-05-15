06755a8d8d52ca63af543b060bd9d20ee298e7fd
Erin Green <erin.y.green@gmail.com>
2019-05-15 17:36:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ class Route {
   }
 
   blocksTravelled() {
-    let horizontalBlocks = this.avenueToInteger(this.endingLocation.horizontal) - this(avenueToInteger(this.beginningLocation.horizontal))
+    let horizontalBlocks = Math.abs(this.avenueToInteger(this.endingLocation.horizontal) - this(avenueToInteger(this.beginningLocation.horizontal)))
     let verticalBlocks = Math.abs(beginningLocation.vertical - endingLocation.vertical)
 
     return horizontalBlocks + verticalBlocks
